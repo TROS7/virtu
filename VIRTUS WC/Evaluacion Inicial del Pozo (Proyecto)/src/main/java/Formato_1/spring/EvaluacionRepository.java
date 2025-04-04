9a501f91ce94851654e0ae501e16e08f56dfeb52
grancar12 <loscamacho19@outlook.com>
2025-04-04 10:03:17 -0500

    Delete VIRTUS WC directory

--- VIRTUS WC/Evaluacion Inicial del Pozo (Proyecto)/src/main/java/Formato_1/spring/EvaluacionRepository.java
+++
@@ -1,8 +0,0 @@
-package Formato_1.spring;
-import Formato_1.spring.Evaluacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluacionRepository extends JpaRepository<Evaluacion, Long> {
-}
